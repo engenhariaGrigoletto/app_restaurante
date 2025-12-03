ea2bfff7404bac45e392df4f0f226f6ecc8a425c
Leonardo Grigoletto <engenharia3@gimi.com.br>
2025-12-03 12:26:51 -0300

    Projeto inicial

---
+++ app.py
@@ -0,0 +1,38 @@
+
+
+def tela_inicial():
+    #Mensagens da tela inicial
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair')
+    
+    while (1):
+        #Pegando dados do usuário
+        try:
+            escolha_usuario = int(input('\nEscolha uma das opções acima:'))
+
+            if escolha_usuario>=1 and escolha_usuario<=4:
+                break
+            else:
+                print('\nInforme um valor dentro do intervalo!')
+                continue
+        except:
+            print('\nInforme um valor válido!')
+            continue
+    
+
+def main():
+    tela_inicial()
+
+#Verificação se a função está dentro do arquivo
+if __name__ == '__main__':
+    main()
